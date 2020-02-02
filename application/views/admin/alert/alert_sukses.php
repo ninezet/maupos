c761edf90a23211550515efad434e2d78eb0220f
Ninezet Dev <ninezet.id@gmail.com>
2020-02-03 00:01:37 +0700

    initial commit

---
+++ application/views/admin/alert/alert_sukses.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Produk By Mahakarya Promosindo">
+    <meta name="author" content="Mahakarya Promosindo">
+
+    <link rel="icon" type="image/png" href="<?= base_url() ?>/assets/img/maulogo.png">
+    <title>Transaksi Penjualan</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url() . 'assets/css/bootstrap.min.css' ?>" rel="stylesheet">
+    <link href="<?php echo base_url() . 'assets/css/style.css' ?>" rel="stylesheet">
+    <link href="<?php echo base_url() . 'assets/css/font-awesome.css' ?>" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url() . 'assets/css/4-col-portfolio.css' ?>" rel="stylesheet">
+    <link href="<?php echo base_url() . 'assets/css/dataTables.bootstrap.min.css' ?>" rel="stylesheet">
+    <link href="<?php echo base_url() . 'assets/css/jquery.dataTables.min.css' ?>" rel="stylesheet">
+    <link href="<?php echo base_url() . 'assets/dist/css/bootstrap-select.css' ?>" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'assets/css/bootstrap-datetimepicker.min.css' ?>">
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <?php
+    $this->load->view('admin/menu');
+    ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="alert alert-success">
+                    <strong>Transaksi Berhasil Silahkan Cetak Faktur Penjualan!</strong>
+                    <a class="btn btn-default" href="<?php echo base_url() . 'admin/penjualan' ?>"><span class="fa fa-backward"></span>Kembali</a>
+                    <a class="btn btn-info" href="<?php echo base_url() . 'admin/penjualan/cetak_faktur' ?>" target="_blank"><span class="fa fa-print"></span>Cetak</a>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+        <!-- Projects Row -->
+
+
+
+        <!--END MODAL-->
+
+        <hr>
+
+        <!-- Footer -->
+        <footer class="main-footer">
+            <div class="pull-right hidden-xs">
+                <b>Version</b> 1.0.0
+            </div>
+            <strong>Copyright &copy; 2019 | <i class="fa fa-whatsapp"></i><a href="https://api.whatsapp.com/send?phone=6281809291404" target="_blank"> +6281-80929-1404 </a>| <i class="fa fa-instagram"></i><a href="https://www.instagram.com/go.zyer/" target="_blank"> Chaizir Maulana </a>| <i class="fa fa-github"></i><a href="https://github.com/ninezet" target="_blank"> ninezet </a> |</strong> All rights
+            reserved.
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url() . 'assets/js/jquery.js' ?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url() . 'assets/dist/js/bootstrap-select.min.js' ?>"></script>
+    <script src="<?php echo base_url() . 'assets/js/bootstrap.min.js' ?>"></script>
+    <script src="<?php echo base_url() . 'assets/js/dataTables.bootstrap.min.js' ?>"></script>
+    <script src="<?php echo base_url() . 'assets/js/jquery.dataTables.min.js' ?>"></script>
+    <script src="<?php echo base_url() . 'assets/js/jquery.price_format.min.js' ?>"></script>
+    <script src="<?php echo base_url() . 'assets/js/moment.js' ?>"></script>
+    <script src="<?php echo base_url() . 'assets/js/bootstrap-datetimepicker.min.js' ?>"></script>
+
+
+
+</body>
+
+</html>
